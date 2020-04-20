8769f4133ef9e705b501f9901e3f6c54a4435be2
morrisseyjsean <morrisseyjsean@gmail.com>
2020-04-20 14:48:50 +0800

    Finished exersice 1.14: Display most voted

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
+const Header = (props) => {
+  return <h1>{props.text}</h1>;
+};
+
 const Anecdote = (props) => {
   return (
     <p>
-      {props.anecdote} <br /> Votes: {props.votes}
+      {anecdotes[props.selected]} <br /> has {props.votes[props.selected]}{" "}
+      votes.
     </p>
   );
 };
@@ -16,11 +21,22 @@ const Button = (props) => {
 const App = (props) => {
   const [selected, setSelected] = useState(0);
   const [votes, setVotes] = useState(new Uint8Array(props.anecdotes.length));
+  const [mostVotes, setMostVotes] = useState(0);
 
   const randomAnecdote = () => {
     const min = 0;
     const max = props.anecdotes.length;
     return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+  };
+
+  const findMostVotes = (newVotes) => {
+    let mostVoted = mostVotes;
+    for (let i = 0; i < props.anecdotes.length; i++) {
+      if (newVotes[i] > newVotes[mostVoted]) {
+        mostVoted = i;
+      }
+    }
+    setMostVotes(mostVoted);
   };
 
   const handleRandomClick = () => {
@@ -31,13 +47,17 @@ const App = (props) => {
     const newVotes = [...votes];
     newVotes[selected] += 1;
     setVotes(newVotes);
+    findMostVotes(newVotes);
   };
 
   return (
     <div>
-      <Anecdote anecdote={props.anecdotes[selected]} votes={votes[selected]} />
+      <Header text="Anecdote of the Day" />
+      <Anecdote selected={selected} votes={votes} />
       <Button handleClick={handleVoteClick} text="Vote" />
       <Button handleClick={handleRandomClick} text="Next anecdote" />
+      <Header text="Most voted Anecdote" />
+      <Anecdote selected={mostVotes} votes={votes} />
     </div>
   );
 };
